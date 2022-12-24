6200ab21c934147fe8c180bff50ba4df99b96ebc
EC2 Default User <ec2-user@ip-172-31-36-110.ap-northeast-1.compute.internal>
2022-12-24 17:17:05 +0000

    [add]Bookers2_アイコンの挿入

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -51,11 +51,11 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
   def correct_user
     @book = Book.find(params[:id])
     @user = @book.user
     redirect_to(root_path) unless @user == current_user
   end
-  
+
 end
