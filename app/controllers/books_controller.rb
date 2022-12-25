273e0e22084baa55f8a91b1d33662f0eceb29a38
EC2 Default User <ec2-user@ip-172-31-36-110.ap-northeast-1.compute.internal>
2022-12-25 03:08:41 +0000

    [update]Bookers2_ユーザー情報の表示を修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,10 +19,12 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @books = Book.all
+    @user = current_user
   end
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def edit
